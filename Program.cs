d58e45fee99a5154ec9e25143227cac3089b3dd6
Dmitry-Osipov <dimaosipov00@gmail.com>
2023-07-25 14:26:05 +0300

    fulfillment code in Program.cs

--- Program.cs
+++ Program.cs
@@ -11,19 +11,43 @@ void PrintArray (string[] arrayToShow)
 {
     for (int i = 0; i < arrayToShow.Length; i += 1)
     {
-        Console.Write(arrayToShow[i]);
-        if (i != arrayToShow.Length - 1)
-        {
-            Console.Write(", ");
-        }
-        else
-        {
-            Console.WriteLine(".");
-        }
-    }
+        Console.Write($"{arrayToShow[i]} ");
+    }    
 }
 
 string[] first_array = {"Hello", "2", "world", ":-)"};
 string[] second_array = {"1234", "1567", "-2", "computer science"};
 string[] third_array = {"Russia", "Denmark", "Kazan"};
+string[] test_array = {"How", "are", "YOU", "my friend", "?", "471", "788902", "Column", "RoW", "Just"};
 
+string[] UpdateArray(string[] arrayToUpdate)
+{
+    int newLength = 0;
+    for (int i = 0; i < arrayToUpdate.Length; i += 1)
+    {
+        if (arrayToUpdate[i].Length <= 3) newLength++;
+    }
+    string[] newArray = new string[newLength];
+    int index = 0;
+    for (int i = 0; i < arrayToUpdate.Length; i += 1)
+    {
+        if (arrayToUpdate[i].Length <= 3)
+        {
+            newArray[index] = arrayToUpdate[i];
+            index++;
+        }
+    }
+    return newArray;
+}
+
+string[] modFirst = UpdateArray(first_array);
+PrintArray(modFirst);
+Console.WriteLine();
+string[] modSecond = UpdateArray(second_array);
+PrintArray(modSecond);
+Console.WriteLine();
+string[] modThird = UpdateArray(third_array);
+PrintArray(modThird);
+Console.WriteLine();
+string[] modTest = UpdateArray(test_array);
+PrintArray(modTest);
